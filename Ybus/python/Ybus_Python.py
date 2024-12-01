dca3a83a054364ae39c2da8daf2107949978be7e
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-01 23:12:44 +0530

    FF

--- Ybus/python/Ybus_Python.py
+++ Ybus/python/Ybus_Python.py
@@ -1,97 +1,91 @@
-"""
-The user needs to keep the linedata matrix into four columns: Bus 1, Bus 2, Resistance, and Reactance.
-Then, replace the zdata matrix with their own. For now, the linedata of the IEEE-30 Bus system is considered.
-The program then calculates the admittance matrix and prints it to the console.
-
-Attributes:
-    zdata (np.array): Linedata matrix with columns: bus1, bus2, Resistance (pu), Reactance (pu).
-    nbr (int): Number of lines or branches.
-    nl (np.array): Starting bus numbers.
-    nr (np.array): Ending bus numbers.
-    R (np.array): Resistance (pu).
-    X (np.array): Reactance (pu).
-    Z (np.array): Impedance.
-    nbus (int): Total number of buses.
-    y (np.array): Admittance.
-    Ybus (np.array): Bus admittance matrix.
-
-Author: Sree Sai Nandini
-"""
-
 import numpy as np
+
+"""
+**Example linedata matrix for IEEE-30 Bus system**
+
+Replace this with your own linedata matrix. 
+Ensure the format is: Bus1, Bus2, Resistance (pu), Reactance (pu)
+"""
+zdata = np.array([
+    [1, 2, 0.02, 0.06],
+    [1, 3, 0.08, 0.24],
+    # ... other lines
+])
+
+# Number of lines or branches
+nbr = len(zdata[:, 1])
+
+# Starting and ending bus numbers
+nl = np.array(zdata[:, 0], dtype=int)
+nr = np.array(zdata[:, 1], dtype=int)
+
+# Resistance and reactance (pu)
+R = zdata[:, 2]
+X = zdata[:, 3]
+
+# Total number of buses
+nbus = int(max(max(nl), max(nr)))
+
+
+def calculate_admittance(R, X):
+    """
+    Calculates the admittance values from resistance and reactance.
+
+    Args:
+        R (np.array): Resistance values.
+        X (np.array): Reactance values.
+
+    Returns:
+        np.array: Admittance values.
+    """
+    Z = R + 1j * X  # Calculate impedance
+    y = 1 / Z  # Calculate admittance
+    return y
+
 
 def calculate_admittance_matrix(zdata):
     """
-    Calculate the bus admittance matrix.
+    Calculates the bus admittance matrix from the linedata.
 
     Args:
-        zdata (np.array): Linedata matrix with columns: bus1, bus2, Resistance (pu), Reactance (pu).
+        zdata (np.array): Linedata matrix.
 
     Returns:
         np.array: Bus admittance matrix.
     """
-    """ @variable nbr Number of lines or branches """
-    nbr = len(zdata[:,1])
-    
-    # Starting bus numbers
-    nl = np.array(zdata[:,0], dtype=int)
-    # Ending bus numbers
-    nr = np.array(zdata[:,1], dtype=int)
-    # Resistance (pu)
-    R = zdata[:,2]
-    # Reactance (pu)
-    X = zdata[:,3]
-    # Impedance
-    Z = np.zeros(nbr, dtype=complex)
-    # Total number of buses
-    nbus = int(max(max(nl), max(nr)))
-    
-    # Calculate Impedance
-    for i in range(len(R)):
-        Z[i] = complex(R[i], X[i])
-    
-    # Calculate Admittance
-    y = 1 / Z
-    # Form bus admittance matrix
+    y = calculate_admittance(R, X)
+
+    # Initialize the bus admittance matrix
     Ybus = np.zeros((nbus, nbus), dtype=complex)
-    
-    # Formation of off diagonal elements
+
+    # Form off-diagonal elements
     for k in range(nbr):
         if nl[k] > 0 and nr[k] > 0:
-            Ybus[nl[k]-1, nr[k]-1] = Ybus[nl[k]-1, nr[k]-1] - y[k]
-            Ybus[nr[k]-1, nl[k]-1] = Ybus[nl[k]-1, nr[k]-1]
-    
-    # Formation of diagonal elements
+            Ybus[nl[k] - 1, nr[k] - 1] -= y[k]
+            Ybus[nr[k] - 1, nl[k] - 1] -= y[k]
+
+    # Form diagonal elements
     for n in range(nbus):
         for k in range(nbr):
-            if nl[k]-1 == n or nr[k]-1 == n:
+            if nl[k] - 1 == n or nr[k] - 1 == n:
                 Ybus[n, n] += y[k]
-    
+
     return Ybus
+
 
 def print_admittance_matrix(Ybus):
     """
-    Print the bus admittance matrix to the console.
+    Prints the bus admittance matrix to the console.
 
     Args:
         Ybus (np.array): Bus admittance matrix.
     """
-    print('Bus Admittance Matrix is:')
-    for i in range(len(Ybus)):
-        for j in range(len(Ybus)):
-            print("{num.real:+0.02f} {num.imag:+0.02f}j".format(num=Ybus[i][j]), end="  ")
+    print('Bus Admittance Matrix:')
+    for row in Ybus:
+        for element in row:
+            print(f"{element.real:+0.02f} {element.imag:+0.02f}j", end="  ")
         print()
 
-# Example linedata matrix for IEEE-30 Bus system
-zdata = np.array([
-    [1, 2, 0.02, 0.06],
-    [1, 3, 0.08, 0.24],
-    [2, 3, 0.06, 0.25],
-    [2, 4, 0.06, 0.18],
-    [2, 5, 0.04, 0.12],
-    [3, 4, 0.01, 0.03],
-    [4, 5, 0.08, 0.24]
-])
 
 # Calculate and print the admittance matrix
 Ybus = calculate_admittance_matrix(zdata)
--- Ybus/python/docs/Ybus_python.py.md
+++ Ybus/python/docs/Ybus_python.py.md
@@ -3,33 +3,50 @@
 <a href="..\..\..\Ybus\python\Ybus_python.py#L0"><img align="right" style="float:right;" src="https://img.shields.io/badge/-source-cccccc?style=flat-square"></a>
 
 # <kbd>module</kbd> `Ybus_python.py`
-The user needs to keep the linedata matrix into four columns: Bus 1, Bus 2, Resistance, and Reactance. Then, replace the zdata matrix with their own. For now, the linedata of the IEEE-30 Bus system is considered. The program then calculates the admittance matrix and prints it to the console. 
 
 
 
-**Attributes:**
- 
- - <b>`zdata`</b> (np.array):  Linedata matrix with columns: bus1, bus2, Resistance (pu), Reactance (pu). 
- - <b>`nbr`</b> (int):  Number of lines or branches. 
- - <b>`nl`</b> (np.array):  Starting bus numbers. 
- - <b>`nr`</b> (np.array):  Ending bus numbers. 
- - <b>`R`</b> (np.array):  Resistance (pu). 
- - <b>`X`</b> (np.array):  Reactance (pu). 
- - <b>`Z`</b> (np.array):  Impedance. 
- - <b>`nbus`</b> (int):  Total number of buses. 
- - <b>`y`</b> (np.array):  Admittance. 
- - <b>`Ybus`</b> (np.array):  Bus admittance matrix. 
-
-Author: Sree Sai Nandini 
 
 **Global Variables**
 ---------------
 - **zdata**
+- **nbr**
+- **nl**
+- **nr**
+- **R**
+- **X**
+- **nbus**
 - **Ybus**
 
 ---
 
-<a href="..\..\..\Ybus\python\Ybus_python.py#L23"><img align="right" style="float:right;" src="https://img.shields.io/badge/-source-cccccc?style=flat-square"></a>
+<a href="..\..\..\Ybus\python\Ybus_python.py#L30"><img align="right" style="float:right;" src="https://img.shields.io/badge/-source-cccccc?style=flat-square"></a>
+
+## <kbd>function</kbd> `calculate_admittance`
+
+```python
+calculate_admittance(R, X)
+```
+
+Calculates the admittance values from resistance and reactance. 
+
+
+
+**Args:**
+ 
+ - <b>`R`</b> (np.array):  Resistance values. 
+ - <b>`X`</b> (np.array):  Reactance values. 
+
+
+
+**Returns:**
+ 
+ - <b>`np.array`</b>:  Admittance values. 
+
+
+---
+
+<a href="..\..\..\Ybus\python\Ybus_python.py#L46"><img align="right" style="float:right;" src="https://img.shields.io/badge/-source-cccccc?style=flat-square"></a>
 
 ## <kbd>function</kbd> `calculate_admittance_matrix`
 
@@ -37,13 +54,13 @@ Author: Sree Sai Nandini
 calculate_admittance_matrix(zdata)
 ```
 
-Calculate the bus admittance matrix. 
+Calculates the bus admittance matrix from the linedata. 
 
 
 
 **Args:**
  
- - <b>`zdata`</b> (np.array):  Linedata matrix with columns: bus1, bus2, Resistance (pu), Reactance (pu). 
+ - <b>`zdata`</b> (np.array):  Linedata matrix. 
 
 
 
@@ -54,7 +71,7 @@ Calculate the bus admittance matrix.
 
 ---
 
-<a href="..\..\..\Ybus\python\Ybus_python.py#L72"><img align="right" style="float:right;" src="https://img.shields.io/badge/-source-cccccc?style=flat-square"></a>
+<a href="..\..\..\Ybus\python\Ybus_python.py#L76"><img align="right" style="float:right;" src="https://img.shields.io/badge/-source-cccccc?style=flat-square"></a>
 
 ## <kbd>function</kbd> `print_admittance_matrix`
 
@@ -62,7 +79,7 @@ Calculate the bus admittance matrix.
 print_admittance_matrix(Ybus)
 ```
 
-Print the bus admittance matrix to the console. 
+Prints the bus admittance matrix to the console. 
 
 
 
