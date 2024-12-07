f351da2f28eb354a4cc325367cab4ccc0afd7a93
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-07 23:30:08 +0530

    wr

--- Ybus/matlab/Ybus_matlab.m
+++ Ybus/matlab/Ybus_matlab.m
@@ -1,11 +1,4 @@
-%% @brief Defines the line data matrix with columns: bus1, bus2, Resistance (pu), Reactance (pu)
-%% 
-%% The line data matrix contains information about the lines in the power system network. 
-%% Each row represents a line with the following columns:
-%% 1. Starting bus number
-%% 2. Ending bus number
-%% 3. Resistance in per unit (pu)
-%% 4. Reactance in per unit (pu)
+%% Linedata Matrix
 zdata = [
     1   2   0.02  0.06;
     1   3   0.08  0.24;
@@ -16,22 +9,22 @@ zdata = [
     4   5   0.08  0.24;
 ];
 
-%% Extract line data
+
 nbr = size(zdata, 1);  % Number of branches
 nl = zdata(:, 1);  % From bus number
 nr = zdata(:, 2);  % To bus number
 R = zdata(:, 3);  % Resistance
-X = zdata(:, 4);  % Reactance
-nbus = max(max(nl), max(nr));  % Number of buses
+X = zdata(:, 4); 
+nbus = max(max(nl), max(nr));
 
-%% Calculate admittance values
+
 Z = R + 1j * X;  % Impedance matrix
 y = 1 ./ Z;  % Admittance matrix
 
 %% Initialize the bus admittance matrix
 Ybus = zeros(nbus, nbus);
 
-%% Create the bus admittance matrix function
+
 function YBUS = create_admittance_matrix(nl, nr, nbr, nbus, y)
     Ybus = zeros(nbus, nbus);  % Initialize inside the function
     for k = 1:nbr
