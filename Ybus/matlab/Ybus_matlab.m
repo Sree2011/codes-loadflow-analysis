f5b5b0d6812ae0327d818085c7319038125529c5
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 13:20:25 +0530

    jj

--- Ybus/matlab/Ybus_matlab.m
+++ Ybus/matlab/Ybus_matlab.m
@@ -7,15 +7,14 @@ choice = input('Enter 1 for impedance and 2 for admittance:');
 % \brief Get the choice from the user - 1 for impedance and 2 for admittance
 
 
-
-% \link ./get_input.m
+% @link ./get_input.m
 y = get_input(choice,n);
 
 
-% \link ./calculate_matrix.m
+% @link ./calculate_matrix.m
 YBUS = calculate_matrix(y, n);
 
 
 
-% \link ./display_matrix.m
+% @link ./display_matrix.m
 display_matrix(YBUS, n);
