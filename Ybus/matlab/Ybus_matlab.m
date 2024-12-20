ceb6f2b329a050a97acabafde079ce483ef18619
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 15:41:57 +0530

    dg

--- Ybus/matlab/Ybus_matlab.m
+++ Ybus/matlab/Ybus_matlab.m
@@ -1,21 +1,22 @@
 
 n = input('Enter the number of buses: ');
-% /// \brief Get the number of buses from the user
+% /// Get the number of buses from the user
 
 
 choice = input('Enter 1 for impedance and 2 for admittance:');
-% /// \brief Get the choice from the user - 1 for impedance and 2 for admittance
+% /// Get the choice from the user - 1 for impedance and 2 for admittance
 
 
-% /// \brief get input
-% /// \link get_input.m
+
 y = get_input(choice,n);
+% /// get input from the user and calculate line admittances
 
 
-% /// \link ./calculate_matrix.m
 YBUS = calculate_matrix(y, n);
+% /// Form the bus admittance matrix based on the input
+% /// \link calculate_matrix.m calculate_matrix \endlink
 
 
 
-% /// \link ./display_matrix.m
 display_matrix(YBUS, n);
+% /// Display the bus admittance matrix
