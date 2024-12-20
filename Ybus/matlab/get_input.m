d6cf7c7489d319c589a1f3fd1ec414c688639d50
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 15:57:05 +0530

    Improve documentation formatting in get_input.m for consistency

--- Ybus/matlab/get_input.m
+++ Ybus/matlab/get_input.m
@@ -1,14 +1,12 @@
+
+% /// \brief Get impedance or admittance input from the user
+% /// \param choice The user's choice for input type : 1 for impedance, 2 for admittance
+% /// \param n The number of buses
+% /// \return yp The admittance matrix
 function yp = get_input(choice,n)
-    % \brief Get impedance or admittance input from the user
-    % \param choice The user's choice for input type : 1 for impedance, 2 for admittance
-    % \param n The number of buses
-    % \return yp The admittance matrix
+
     
-
-
     y = zeros(n, n);
-
-    
     if choice == 1
         for i = 1:n
             for j = 1:n
