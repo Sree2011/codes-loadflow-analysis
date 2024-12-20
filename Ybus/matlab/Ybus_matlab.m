1156db9956af9254f5338a55238e7a011f336edf
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 15:47:32 +0530

    Refactor Ybus_matlab.m for improved readability and consistency

--- Ybus/matlab/Ybus_matlab.m
+++ Ybus/matlab/Ybus_matlab.m
@@ -1,24 +1,25 @@
-
-n = input('Enter the number of buses: ');
 % /// Get the number of buses from the user
+n = input('Enter the number of buses: ');
 
 
-choice = input('Enter 1 for impedance and 2 for admittance:');
 % /// Get the choice from the user - 1 for impedance and 2 for admittance
+choice = input('Enter 1 for impedance and 2 for admittance:');
 
 
 
-y = get_input(choice,n);
 % /// get input from the user and calculate line admittances
 % /// \link get_input.m get_input \endlink
+y = get_input(choice,n);
 
 
-YBUS = calculate_matrix(y, n);
+
 % /// Form the bus admittance matrix based on the input
 % /// \link calculate_matrix.m calculate_matrix \endlink
+YBUS = calculate_matrix(y, n);
 
 
 
-display_matrix(YBUS, n);
 % /// Display the bus admittance matrix
 % /// \link display_matrix.m display_matrix \endlink
+display_matrix(YBUS, n);
+
