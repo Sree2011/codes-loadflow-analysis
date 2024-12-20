03eeb7e6169dcd2e435d1e3cafbba5760aa59217
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 12:59:49 +0530

    yj

--- Ybus/matlab/calculate_matrix.m
+++ Ybus/matlab/calculate_matrix.m
@@ -1,12 +1,12 @@
-function YBus = calculate_matrix(y,n)
-    %{
+%{
+    \fn calculate_matrix(y,n)
     \brief Calculate the bus admittance matrix
     \arg y The input admittance matrix
     \arg n The number of buses
     \return YBus The calculated bus admittance matrix
-    %}
+%}
+function YBus = calculate_matrix(y,n)
 
-    
     Ybus = zeros(n, n);
     % \brief Initialise the bus admittance matrix
     %
