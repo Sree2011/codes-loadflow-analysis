47f5e5a370d24d314c9c46ed00b24ee0840114ea
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 15:38:14 +0530

    fb

--- Ybus/matlab/Ybus_matlab.m
+++ Ybus/matlab/Ybus_matlab.m
@@ -1,21 +1,21 @@
 
 n = input('Enter the number of buses: ');
-% \brief Get the number of buses from the user
+% /// \brief Get the number of buses from the user
 
 
 choice = input('Enter 1 for impedance and 2 for admittance:');
-% \brief Get the choice from the user - 1 for impedance and 2 for admittance
+% /// \brief Get the choice from the user - 1 for impedance and 2 for admittance
 
 
-% \brief get input
-% \link get_input.m
+% /// \brief get input
+% /// \link get_input.m
 y = get_input(choice,n);
 
 
-% @link ./calculate_matrix.m
+% /// \link ./calculate_matrix.m
 YBUS = calculate_matrix(y, n);
 
 
 
-% @link ./display_matrix.m
+% /// \link ./display_matrix.m
 display_matrix(YBUS, n);
