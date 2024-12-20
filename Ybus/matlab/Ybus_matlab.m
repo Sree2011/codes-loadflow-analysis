2f8c2304db53f291356e4b0187b6770ec18f61b6
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 13:17:51 +0530

    rg

--- Ybus/matlab/Ybus_matlab.m
+++ Ybus/matlab/Ybus_matlab.m
@@ -7,27 +7,15 @@ choice = input('Enter 1 for impedance and 2 for admittance:');
 % \brief Get the choice from the user - 1 for impedance and 2 for admittance
 
 
-%{
-    \fn get_input(choice,n)
-    \brief Calculate the bus admittance matrix
-    \arg y The input admittance matrix
-    \arg n The number of buses
-    \return YBus The calculated bus admittance matrix
-    \link get_input.m
-%}
 
+% \link ./get_input.m
 y = get_input(choice,n);
 
-%{
-    \brief Calculate the bus admittance matrix
-    \arg y The input admittance matrix
-    \arg n The number of buses
-    \return YBus The calculated bus admittance matrix
-    \link calculate_matrix.m
-%}
+
+% \link ./calculate_matrix.m
 YBUS = calculate_matrix(y, n);
 
 
 
-% \link display_matrix.m
+% \link ./display_matrix.m
 display_matrix(YBUS, n);
