4c48185897655b1aaed01e486d919b98accb6ab1
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-12-20 15:53:22 +0530

    Enhance documentation in display_matrix.m for clarity and consistency

--- Ybus/matlab/display_matrix.m
+++ Ybus/matlab/display_matrix.m
@@ -1,18 +1,21 @@
+
+% /// \brief Display the bus admittance matrix
+% /// \param Ybus The bus admittance matrix
+% /// \param nbus The number of buses
 function display_matrix(Ybus, nbus)
-    %{
-    \brief Display the bus admittance matrix
-    \param Ybus The bus admittance matrix
-    \param nbus The number of buses
-    %}
-
-
     disp('Bus Admittance Matrix:')
+
+    % /// \brief external loop
     for i = 1:nbus
-        % \brief external loop
+        
+        % /// \brief internal loop
         for j = 1:nbus
-            % \brief external loop
+            
+            % /// \brief Display the real and imaginary parts of the bus admittance matrix
             fprintf('%.2f + %.2fj\t', real(Ybus(i, j)), imag(Ybus(i, j)));
         end
+
+        % /// \brief Move to the next line
         fprintf('\n');
     end
 end
